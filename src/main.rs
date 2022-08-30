095370e2bb5a844c1d66b184050c846f770346c6
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-08-30 17:58:00 -0400

    Add draw example to demo

--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,17 @@ fn main() {
     *board.space_mut(BoardSpaceLocation::BottomRight) = BoardSpace::O;
     println!("TopLeft to BottomRight O: {}", board);
     println!("Winner: {:?}", GameOutcome::analyze_game(&board));
+
+    *board.space_mut(BoardSpaceLocation::TopLeft) = BoardSpace::O;
+    *board.space_mut(BoardSpaceLocation::TopMiddle) = BoardSpace::X;
+    *board.space_mut(BoardSpaceLocation::TopRight) = BoardSpace::O;
+    *board.space_mut(BoardSpaceLocation::MiddleLeft) = BoardSpace::X;
+    *board.space_mut(BoardSpaceLocation::MiddleMiddle) = BoardSpace::O;
+    *board.space_mut(BoardSpaceLocation::MiddleRight) = BoardSpace::X;
+    *board.space_mut(BoardSpaceLocation::BottomLeft) = BoardSpace::X;
+    *board.space_mut(BoardSpaceLocation::BottomMiddle) = BoardSpace::O;
+    *board.space_mut(BoardSpaceLocation::BottomRight) = BoardSpace::X;
+    println!("Draw example: {}", board);
+    println!("Winner: {:?}", GameOutcome::analyze_game(&board));
+    
 }
