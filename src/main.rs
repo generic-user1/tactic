06f6ed5bdf266aeb79cd336d14c605d18e33ff9c
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-08-30 17:48:20 -0400

    Refactor winner module
    Add GameOutcome enum
    Move functionality of winner module to methods of GameOutcome

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use tactic::gameboard::{GameBoard, BoardSpace, BoardSpaceLocation};
-use tactic::winner;
+use tactic::winner::GameOutcome;
 fn main() {
     let mut board = GameBoard::new();
     
     println!("Default: {}", board);
-    println!("Winner: {:?}\n", winner::get_winner(&board));
+    println!("Winner: {:?}\n", GameOutcome::analyze_game(&board));
 
     for location in BoardSpaceLocation::all(){
         *board.space_mut(location) = BoardSpace::X;
     }
     println!("All Xs: {}", board);
-    println!("Winner: {:?}\n", winner::get_winner(&board));
+    println!("Winner: {:?}\n", GameOutcome::analyze_game(&board));
 
     *board.space_mut(BoardSpaceLocation::TopLeft) = BoardSpace::O;
     *board.space_mut(BoardSpaceLocation::MiddleMiddle) = BoardSpace::O;
     *board.space_mut(BoardSpaceLocation::BottomRight) = BoardSpace::O;
     println!("TopLeft to BottomRight O: {}", board);
-    println!("Winner: {:?}", winner::get_winner(&board));
+    println!("Winner: {:?}", GameOutcome::analyze_game(&board));
 }
--- src/winner.rs
+++ src/winner.rs
@@ -4,11 +4,96 @@ use crate::gameboard::{GameBoard, BoardSpace, BoardSpaceLocation};
 
 mod win_position_constants;
 
+/// The outcome of a game, if any
+/// 
+/// The `PlayerX` and `PlayerO` variants represent that the game
+/// was won by the indicated player, and include the [WinPosition] the game was won with.
+/// 
+/// The `Draw` variant represents that the game is finished (no more moves can be played)
+/// but that there was no winner.
+/// 
+/// The `Incomplete` variant represents that neither player has won, but that there
+/// are still moves that can be played.
+#[derive(Debug)]
+pub enum GameOutcome{
+    PlayerX(WinPosition),
+    PlayerO(WinPosition),
+    Draw,
+    Incomplete
+}
+impl GameOutcome {
+    
+    /// Analyzes a given [GameBoard] for a winner
+    /// 
+    ///# Notes
+    /// 
+    /// If a game has multiple valid win positions,
+    /// only one win position (and therefore one winner) is selected.
+    /// The prioritiy of win positions is defined by the order that [WinPosition] variants
+    /// are defined in; the earlier variants are higher priority than the later variants.
+    /// 
+    /// More specifically, the priority is defined by the order that variants are returned by
+    /// the [WinPosition::all] function, but this order and the variant definition order 
+    /// should be identical.
+    pub fn analyze_game(board: &GameBoard) -> GameOutcome
+    {
+        // initialize a flag tracking the presence of empty spaces to false
+        // if any empty spaces are found, this will be set to true
+        let mut empty_spaces_present = false;
+        for win_position in WinPosition::all(){
+
+            //get iter over the BoardSpace in each position
+            let mut board_space_values = 
+                win_position.as_board_spaces().iter().map(|board_space|{
+                    board.space(*board_space)
+                });
+            
+            // consume first value from iter and store as possible winner
+            let possible_winner = board_space_values.next().unwrap();
+            // set is_winner to true if the rest of values from iter match possible_winner
+            let is_winner = board_space_values.all(|board_space|{
+                board_space == possible_winner});
+
+            if is_winner{
+                match possible_winner {
+                    BoardSpace::Empty => {empty_spaces_present = true},
+                    BoardSpace::X => {return GameOutcome::PlayerX(win_position);},
+                    BoardSpace::O => {return GameOutcome::PlayerO(win_position);}
+                }
+            }
+        }
+
+        // At this point, we have determined that neither player has won, 
+        // as all win positions have been checked and no winner was found.
+        // The return value will now be Incomplete if empty spaces were found,
+        // or Draw if no empty spaces were found (indicating no more possible moves)
+        if empty_spaces_present {
+            GameOutcome::Incomplete
+        } else {
+            GameOutcome::Draw
+        }
+    }
+
+    /// Returns `true` if the game is finished
+    /// 
+    /// The game is finished if there are no more moves to be played or a player has won.
+    pub fn game_finished(&self) -> bool
+    {
+        !matches!(self, Self::Incomplete)  
+    }
+
+    /// Returns `true` if the game has been won
+    pub fn game_won(&self) -> bool
+    {
+        !matches!(self, Self::Draw | Self::Incomplete)
+    }
+}
+
 /// The row, column, or diagonal that a game was won with
 /// 
 ///# Notes
 /// 
-/// The [get_winner] function will check each win position in the order
+/// The [GameOutcome::analyze_game] function will check each win position in the order
 /// that `WinPosition` variants are defined.
 #[derive(Debug)]
 pub enum WinPosition {
@@ -23,7 +108,7 @@ pub enum WinPosition {
 }
 
 impl WinPosition{
-    /// Returns an array of [BoardSpacePosition] with each space contained in this `WinPosition`
+    /// Returns an array of [BoardSpaceLocation] with each space contained in this `WinPosition`
     pub const fn as_board_spaces(&self) -> &'static [BoardSpaceLocation; 3]
     {
         match self {
@@ -54,42 +139,4 @@ impl WinPosition{
 
         VARIANTS.into_iter()
     }
-}
-
-/// Analyzes a given [GameBoard] for a winner
-/// 
-/// The results of analysis are returned as a tuple `(winning_player, win_position)`.
-/// 
-/// The winning player is represented as a [BoardSpace] variant; 
-/// [BoardSpace::X] if player X won, [BoardSpace::O] if player O won, 
-/// or [BoardSpace::Empty] if neither player won.
-/// 
-/// The win position is represented as an optional [WinPosition]; 
-/// `Some(WinPosition)` if a winner is found or `None` if no winner is found.
-pub fn get_winner(board: &GameBoard) -> (BoardSpace, Option<WinPosition>)
-{
-    for win_position in WinPosition::all(){
-
-        //get iter over the BoardSpace in each position
-        let mut board_space_values = 
-            win_position.as_board_spaces().iter().map(|board_space|{
-                board.space(*board_space)
-            });
-        
-        // consume first value from iter and store as possible winner
-        let possible_winner = board_space_values.next().unwrap();
-        // is_winner is set to true if possible_winner is not Empty
-        // AND if all other values from iter match possible winner
-        let is_winner = possible_winner != &BoardSpace::Empty && 
-            board_space_values.all(|board_space|{
-                board_space == possible_winner
-            });
-        if is_winner {
-            return (possible_winner.clone(), Some(win_position));
-        }
-
-    }
-
-    // return this if all win positions were checked and no winner was found
-    (BoardSpace::Empty, None)
 }
