0206e717aab97903687bd55980a55a61a5276b2b
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-08-31 20:02:19 -0400

    Move event related UI methods to seperate module

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@
 use std::io::{stdout, Write};
 
 use crate::{
-    gameboard::{GameBoard, BoardSpaceLocation, BoardSpace},
+    gameboard::{GameBoard, BoardSpaceLocation},
     player_type::PlayerType,
     game_outcome::GameOutcome,
     active_player::ActivePlayer,
@@ -12,13 +12,16 @@ use crate::{
 use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, Clear, ClearType},
     style::Print,
-    event::{self, Event, KeyEvent, KeyCode, KeyModifiers},
     cursor::{self, MoveToNextLine, MoveToColumn, MoveToRow},
     QueueableCommand, ExecutableCommand
 };
 
 const TERMSIZE_MIN_X: u16 = 11;
 const TERMSIZE_MIN_Y: u16 = 8;
+
+//declare event_handling module which contains
+//event handling impl's for the UI struct
+mod event_handling;
 
 /// Struct used to manage the game UI
 /// 
@@ -251,151 +254,6 @@ impl UI{
         self.cursor_y_pos = 1;
     }
 
-    /// Move cursor to the right (positive x) if possible
-    /// 
-    /// Returns `true` if successful, `false` if not
-    fn move_cursor_right(&mut self) -> bool
-    {
-        if self.cursor_x_pos < 2{
-            self.cursor_x_pos += 1;
-            true
-        } else {
-            false
-        }
-    }
-
-    /// Move cursor to the left (negative x) if possible
-    /// 
-    /// Returns `true` if successful, `false` if not
-    fn move_cursor_left(&mut self) -> bool
-    {
-        if self.cursor_x_pos > 0{
-            self.cursor_x_pos -= 1;
-            true
-        } else {
-            false
-        }   
-    }
-
-    /// Move cursor downwards (positive y) if possible
-    /// 
-    /// Returns `true` if successful, `false` if not
-    fn move_cursor_down(&mut self) -> bool
-    {
-        if self.cursor_y_pos < 2{
-            self.cursor_y_pos += 1;
-            true
-        } else {
-            false
-        }   
-    }
-
-    /// Move cursor upwards (negative y) if possible
-    /// 
-    /// Returns `true` if successful, `false` if not
-    fn move_cursor_up(&mut self) -> bool
-    {
-        if self.cursor_y_pos > 0{
-            self.cursor_y_pos -= 1;
-            true
-        } else {
-            false
-        }
-    }
-
-    /// Claim the selected space for the active player if possible
-    /// 
-    /// Returns `true` if successful, `false` if not
-    fn claim_space(&mut self) -> bool
-    {
-        let desired_location = 
-            BoardSpaceLocation::from_coordinates((self.cursor_x_pos, self.cursor_y_pos));
-        let desired_space = 
-            self.game_board.space_mut(desired_location);
-        
-        // only update space and switch players if selected space is empty
-        if desired_space == &BoardSpace::Empty {
-            //write active player letter to this space
-            *desired_space = match self.active_player {
-                ActivePlayer::PlayerX => BoardSpace::X,
-                ActivePlayer::PlayerO => BoardSpace::O
-            };
-            true
-        } else {
-            false
-        }
-    }
-
-    /// Switches the active player and resets cursor position
-    fn switch_active_player(&mut self) 
-    {
-        //switch player
-        self.active_player.switch();
-
-        //reset cursor position
-        self.reset_cursor_pos();
-    }
-
-    /// Blocks until a [crossterm::event::Event] is available, then handles it
-    fn handle_next_event(&mut self) -> crossterm::Result<()>
-    {
-        match event::read()? {
-            Event::Key(key_event) => {
-                match key_event {
-                    KeyEvent{code:KeyCode::Right, ..} => {
-                        self.move_cursor_right();
-                    },
-                    KeyEvent{code:KeyCode::Left, ..} => {
-                        self.move_cursor_left();
-                    },
-                    KeyEvent{code:KeyCode::Down, ..} => {
-                        self.move_cursor_down();
-                    },
-                    KeyEvent{code:KeyCode::Up, ..} => {
-                        self.move_cursor_up();
-                    },
-                    KeyEvent{code:KeyCode::Enter, ..} => {
-                        //attempt to claim space and switch turns if successful
-                        if self.claim_space(){
-                            self.switch_active_player();
-                        }
-                    },
-                    KeyEvent{code:KeyCode::Char('x'), ..} => {
-                        //attempt to claim space if active player is X
-                        if self.active_player == ActivePlayer::PlayerX && self.claim_space(){
-                            self.switch_active_player();
-                        }
-                    },
-                    KeyEvent{code:KeyCode::Char('o'), ..} => {
-                        //attempt to claim space if active player is O
-                        if self.active_player == ActivePlayer::PlayerO && self.claim_space(){
-                            self.switch_active_player();
-                        }
-                    },
-                    KeyEvent{code:KeyCode::Char('q'), ..} => {
-                        self.exit_flag = true;
-                    },
-                    KeyEvent{code:KeyCode::Char('c'), modifiers:KeyModifiers::CONTROL, ..} => {
-                        self.exit_flag = true;
-                    }
-                    _ => {
-                        //ignore other KeyEvents
-                    }
-                }
-            },
-            Event::Resize(new_x, new_y) => {
-                self.terminal_x_size = new_x;
-                self.terminal_y_size = new_y;
-                stdout().execute(Clear(ClearType::All))?;
-            }
-            _ => {
-                //ignore other Events
-            }
-        }
-
-        Ok(())
-    }
-
     /// Returns the PlayerType of the currently active player
     fn active_player_type(&self) -> &PlayerType
     {
@@ -428,4 +286,3 @@ impl Default for UI {
         }
     }
 }
-
---
+++ src/ui/event_handling.rs
@@ -0,0 +1,160 @@
+//! UI implementations for event handling
+
+use crossterm::{
+    event::{self, Event, KeyEvent, KeyCode, KeyModifiers},
+    terminal::{Clear, ClearType},
+    ExecutableCommand
+};
+use crate::{
+    gameboard::{BoardSpaceLocation, BoardSpace},
+    active_player::ActivePlayer
+};
+use std::io::stdout;
+
+impl super::UI {
+
+    /// Move cursor to the right (positive x) if possible
+    /// 
+    /// Returns `true` if successful, `false` if not
+    pub(super) fn move_cursor_right(&mut self) -> bool
+    {
+        if self.cursor_x_pos < 2{
+            self.cursor_x_pos += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Move cursor to the left (negative x) if possible
+    /// 
+    /// Returns `true` if successful, `false` if not
+    pub(super) fn move_cursor_left(&mut self) -> bool
+    {
+        if self.cursor_x_pos > 0{
+            self.cursor_x_pos -= 1;
+            true
+        } else {
+            false
+        }   
+    }
+
+    /// Move cursor downwards (positive y) if possible
+    /// 
+    /// Returns `true` if successful, `false` if not
+    pub(super) fn move_cursor_down(&mut self) -> bool
+    {
+        if self.cursor_y_pos < 2{
+            self.cursor_y_pos += 1;
+            true
+        } else {
+            false
+        }   
+    }
+
+    /// Move cursor upwards (negative y) if possible
+    /// 
+    /// Returns `true` if successful, `false` if not
+    pub(super) fn move_cursor_up(&mut self) -> bool
+    {
+        if self.cursor_y_pos > 0{
+            self.cursor_y_pos -= 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Claim the selected space for the active player if possible
+    /// 
+    /// Returns `true` if successful, `false` if not
+    pub(super) fn claim_space(&mut self) -> bool
+    {
+        let desired_location = 
+            BoardSpaceLocation::from_coordinates((self.cursor_x_pos, self.cursor_y_pos));
+        let desired_space = 
+            self.game_board.space_mut(desired_location);
+        
+        // only update space and switch players if selected space is empty
+        if desired_space == &BoardSpace::Empty {
+            //write active player letter to this space
+            *desired_space = match self.active_player {
+                ActivePlayer::PlayerX => BoardSpace::X,
+                ActivePlayer::PlayerO => BoardSpace::O
+            };
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Switches the active player and resets cursor position
+    pub(super) fn switch_active_player(&mut self) 
+    {
+        //switch player
+        self.active_player.switch();
+
+        //reset cursor position
+        self.reset_cursor_pos();
+    }
+
+    /// Blocks until a [crossterm::event::Event] is available, then handles it
+    pub(super) fn handle_next_event(&mut self) -> crossterm::Result<()>
+    {
+        match event::read()? {
+            Event::Key(key_event) => {
+                match key_event {
+                    KeyEvent{code:KeyCode::Right, ..} => {
+                        self.move_cursor_right();
+                    },
+                    KeyEvent{code:KeyCode::Left, ..} => {
+                        self.move_cursor_left();
+                    },
+                    KeyEvent{code:KeyCode::Down, ..} => {
+                        self.move_cursor_down();
+                    },
+                    KeyEvent{code:KeyCode::Up, ..} => {
+                        self.move_cursor_up();
+                    },
+                    KeyEvent{code:KeyCode::Enter, ..} => {
+                        //attempt to claim space and switch turns if successful
+                        if self.claim_space(){
+                            self.switch_active_player();
+                        }
+                    },
+                    KeyEvent{code:KeyCode::Char('x'), ..} => {
+                        //attempt to claim space if active player is X
+                        if self.active_player == ActivePlayer::PlayerX && self.claim_space(){
+                            self.switch_active_player();
+                        }
+                    },
+                    KeyEvent{code:KeyCode::Char('o'), ..} => {
+                        //attempt to claim space if active player is O
+                        if self.active_player == ActivePlayer::PlayerO && self.claim_space(){
+                            self.switch_active_player();
+                        }
+                    },
+                    KeyEvent{code:KeyCode::Char('q'), ..} => {
+                        self.exit_flag = true;
+                    },
+                    KeyEvent{code:KeyCode::Char('c'), modifiers:KeyModifiers::CONTROL, ..} => {
+                        self.exit_flag = true;
+                    }
+                    _ => {
+                        //ignore other KeyEvents
+                    }
+                }
+            },
+            Event::Resize(new_x, new_y) => {
+                self.terminal_x_size = new_x;
+                self.terminal_y_size = new_y;
+                stdout().execute(Clear(ClearType::All))?;
+            }
+            _ => {
+                //ignore other Events
+            }
+        }
+
+        Ok(())
+    }
+}
