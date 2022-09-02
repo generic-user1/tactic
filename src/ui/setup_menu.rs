3a286d0446f7d82c4f9aeda8cec9258f1c39b7d0
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-09-02 08:32:40 -0400

    Setup menu notes on possible implementation

--- src/ui/setup_menu.rs
+++ src/ui/setup_menu.rs
@@ -30,3 +30,36 @@
 //! 
 //!  - AI difficulty is reversed in this mode (if player inputs a 1, a 0 
 //!    should be passed to the AiPlayer instance) 
+//!
+//! Menu should be a list of interactable options, where some keys (probably left and right arrows)
+//! allow the user to change the setting, and up/down allow the user to select different settings to change
+//! 
+//! If we have lots of options, there will need to be scrolling support. Selecting an option that is too high
+//! or too low to be viewable should scroll the entire menu. This could be handled with some kind of dynamic offset.
+//! 
+//! Option types would include:
+//!
+//! - "radio buttons" (select one option from a pre-defined set)
+//!
+//!   - toggles could be implemented as a 2-state "radio button" 
+//! 
+//!   - options could be implemented as enums with some method that gives a name to each option
+//! 
+//!   - may want 'descriptions' for some options, where there is some pre-defined space to print 
+//!     a description of the currently selected option.
+//! 
+//! - numeric inputs (for both floats and ints)
+//!   
+//!   - bounds need to be configurable
+//! 
+//!   - float input could take an int from a known range in and scale it to some float output (0-100 scales to 0.0-1.0)
+//!  
+//! 
+//! Options need to be capable of being 'hidden' or 'deactivated'
+//! 
+//! - either no display or a differently styled display to indicate the option is deactivated
+//! 
+//! - a deactivated option may or may not still be selectable, but should be unchangeable
+//! 
+//! 
+//! Selected options need to be highlighted somehow, probably by filling the background of their text.
