3945e48f2fadd5b81e6780d13b80bff3250f5323
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-09-02 10:38:06 -0400

    Add beginnings of SetupMenu
    Concrete types still needed, currently cfg-d out to allow compiliation
    Warnings at this point are expected

--- src/ui/setup_menu.rs
+++ src/ui/setup_menu.rs
@@ -63,3 +63,95 @@
 //! 
 //! 
 //! Selected options need to be highlighted somehow, probably by filling the background of their text.
+
+use crate::{player_type::PlayerType, ai::AiPlayer};
+
+/// Representation of the state of the menu
+/// 
+///# Notes
+/// 
+/// This should only ever be used by the UI struct, as its constructor and destructor
+/// take care of setup and cleanup tasks.
+#[cfg(not_yet_implemented)]
+struct SetupMenu {
+    /// the AiPlayer instance returned here isn't used,
+    /// it will be created if appropriate using the 'player_x_difficulty' option
+    player_x_type: MenuOption<PlayerType>,
+
+    /// the AiPlayer instance returned here isn't used,
+    /// it will be created if appropriate using the 'player_o_difficulty' option
+    player_o_type: MenuOption<PlayerType>,
+
+    /// only used if player x is AI
+    player_x_ai: MenuOption<AiPlayer>,
+
+    /// only used if player o is AI
+    player_o_ai: MenuOption<AiPlayer>,
+
+    autoquit_mode: MenuOption<GameAutoquitMode>,
+
+    game_mode: DescribedMenuOption<GameMode>
+    
+}
+
+/// Determines the game mode to be played
+#[derive(Default)]
+enum GameMode{
+    #[default]
+    Classic,
+    Reverse
+}
+
+/// Determines how many games will be played before auto-exiting
+#[derive(Default)]
+enum GameAutoquitMode {
+    /// No limit
+    #[default]
+    Unlimited,
+    /// Limit the total number of games
+    GameNumberLimit(u32),
+    /// Limit the number of games that are not draws
+    NonDrawNumberLimit(u32),
+    /// Limit the score of either player
+    ScoreNumberLimit(u32)
+}
+
+/// Menu option; allows user to configure some value
+trait MenuOption<T> {
+
+    /// Returns the display name of this option 
+    /// 
+    /// "Player X type", "AI difficulty", etc
+    fn option_name(&self) -> String;
+
+    /// Returns the display name of the currently selected 'value' for this option
+    /// 
+    /// Enums should select the next variant. Numerics should select the next value.
+    fn current_value_name(&self) -> String;
+
+    /// Changes the option to the next value 
+    /// 
+    /// This should switch to the 'next' value if possible
+    /// 
+    /// Returns Ok if this happens correctly, or Err if it doesn't (usually because we are already at maximum)
+    fn next_value(&mut self) -> Result<(),()>;
+
+    /// Changes the option to the previous value 
+    /// 
+    /// This should switch to the 'previous' value if possible
+    /// 
+    /// Returns Ok if this happens correctly, or Err if it doesn't (usually because we are already at maximum)
+    fn prev_value(&mut self) -> Result<(),()>;
+
+    /// Returns the currently selected value
+    fn value(&self) -> T;
+
+}
+
+/// [MenuOption] with an added description of the currently selected value
+trait DescribedMenuOption<T>: MenuOption<T> {
+
+    /// Returns a description of the currently selected value
+    fn description(&self) -> String;
+
+}
