1292da27b2797c180c8aba9b2edd3da2bd759585
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-09-06 18:54:07 -0400

    Implement the rest of the MenuOption structs

--- src/ui/setup_menu.rs
+++ src/ui/setup_menu.rs
@@ -72,7 +72,6 @@ use crate::{active_player::ActivePlayer, player_type::PlayerType, ai::AiPlayer};
 /// 
 /// This should only ever be used by the UI struct, as its constructor and destructor
 /// take care of setup and cleanup tasks.
-#[cfg(not_yet_implemented)]
 struct SetupMenu {
     /// the AiPlayer instance returned here isn't used,
     /// it will be created if appropriate using the 'player_x_difficulty' option
@@ -88,12 +87,182 @@ struct SetupMenu {
     /// only used if player o is AI
     player_o_ai: DifficultyMenuOption,
 
-    autoquit_mode: MenuOption<GameAutoquitMode>,
+    autoquit_mode: AutoquitModeMenuOption,
 
-    autoquit_value: MenuOption<u32>,
+    autoquit_value: AutoquitValueMenuOption,
 
-    game_mode: DescribedMenuOption<GameMode>
+    game_mode: GameModeMenuOption
     
+}
+
+struct GameModeMenuOption {
+    selected_game_mode: GameMode
+}
+
+impl GameModeMenuOption {
+    
+    /// Creates and returns a new GameModeMenuOption for the specified player
+    pub fn new() -> Self
+    {
+        Self{selected_game_mode: GameMode::Classic}
+    }
+}
+
+impl DescribedMenuOption<GameMode> for GameModeMenuOption {
+    fn description(&self) -> String {
+        match self.selected_game_mode {
+            GameMode::Classic => "Play to place three of your pieces in a row. ".to_owned() 
+            + "Prevent your opponent from placing three of their pieces in a row.",
+            GameMode::Reverse => "Play to avoid placing three of your pieces in a row. ".to_owned() 
+            + "Try to force your opponent to place three of their pieces in a row."
+        }
+    }
+}
+
+impl MenuOption<GameMode> for GameModeMenuOption {
+    fn value(self) -> GameMode {
+        self.selected_game_mode
+    }
+
+    fn option_name(&self) -> String {
+        "Game Mode".to_owned()
+    }
+
+    fn current_value_name(&self) -> String {
+        match self.selected_game_mode {
+            GameMode::Classic => "Classic".to_owned(),
+            GameMode::Reverse => "Reverse".to_owned()
+        }
+    }
+
+    fn next_value(&mut self) -> Result<(),()> {
+        match self.selected_game_mode {
+            GameMode::Classic => self.selected_game_mode = GameMode::Reverse,
+            GameMode::Reverse => self.selected_game_mode = GameMode::Classic
+        }
+        Ok(())
+    }
+
+    fn prev_value(&mut self) -> Result<(),()> {
+        self.next_value()
+    }
+}
+
+struct AutoquitValueMenuOption {
+    selected_value: u32
+}
+
+impl AutoquitValueMenuOption {
+    const AUTOQUIT_VALUE_STEP: u32 = 1;
+
+    /// Creates and returns a new AutoquitValueMenuOption for the specified player
+    pub fn new() -> Self
+    {
+        Self{selected_value: 1}
+    }
+}
+
+impl MenuOption<u32> for AutoquitValueMenuOption {
+    fn value(self) -> u32 {
+        self.selected_value
+    }
+
+    fn option_name(&self) -> String {
+        "Game Limit Value".to_owned()
+    }
+
+    fn current_value_name(&self) -> String {
+        format!("{}", self.selected_value)
+    }
+
+    fn next_value(&mut self) -> Result<(),()> {
+        if let Some(new_value) = self.selected_value.checked_add(Self::AUTOQUIT_VALUE_STEP) {
+            self.selected_value = new_value;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    fn prev_value(&mut self) -> Result<(),()> {
+        if let Some(new_value) = self.selected_value.checked_sub(Self::AUTOQUIT_VALUE_STEP) {
+            if new_value > 0 {
+                self.selected_value = new_value;
+                Ok(())
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+}
+
+struct AutoquitModeMenuOption{
+    selected_mode: GameAutoquitMode
+}
+
+impl AutoquitModeMenuOption{
+    /// Creates and returns a new AutoquitModeMenuOption for the specified player
+    pub fn new() -> Self
+    {
+        Self{selected_mode:GameAutoquitMode::Unlimited}
+    }
+}
+
+impl MenuOption<GameAutoquitMode> for AutoquitModeMenuOption {
+    fn value(self) -> GameAutoquitMode {
+        self.selected_mode
+    }
+
+    fn option_name(&self) -> String {
+        "Game Limit Type".to_owned()
+    }
+
+    fn current_value_name(&self) -> String {
+        match self.selected_mode {
+            GameAutoquitMode::Unlimited => "Unlimited".to_owned(),
+            GameAutoquitMode::GameNumberLimit => "Max number of total games".to_owned(),
+            GameAutoquitMode::NonDrawNumberLimit => "Max number of won games".to_owned(),
+            GameAutoquitMode::ScoreNumberLimit => "Max score of either player".to_owned()
+        }
+    }
+
+    fn next_value(&mut self) -> Result<(),()> {
+        match self.selected_mode {
+            GameAutoquitMode::Unlimited => {
+                self.selected_mode = GameAutoquitMode::GameNumberLimit;
+            },
+            GameAutoquitMode::GameNumberLimit => {
+                self.selected_mode = GameAutoquitMode::NonDrawNumberLimit;
+            },
+            GameAutoquitMode::NonDrawNumberLimit => {
+                self.selected_mode = GameAutoquitMode::ScoreNumberLimit;
+            },
+            GameAutoquitMode::ScoreNumberLimit => {
+                return Err(());
+            }
+        }
+        Ok(())
+    }
+
+    fn prev_value(&mut self) -> Result<(),()> {
+        match self.selected_mode {
+            GameAutoquitMode::Unlimited => {
+                return Err(());
+            },
+            GameAutoquitMode::GameNumberLimit => {
+                self.selected_mode = GameAutoquitMode::Unlimited
+            },
+            GameAutoquitMode::NonDrawNumberLimit => {
+                self.selected_mode = GameAutoquitMode::GameNumberLimit;
+            },
+            GameAutoquitMode::ScoreNumberLimit => {
+                self.selected_mode = GameAutoquitMode::NonDrawNumberLimit
+            }
+        }
+        Ok(())
+    }
 }
 
 struct DifficultyMenuOption {
@@ -104,7 +273,7 @@ struct DifficultyMenuOption {
 impl DifficultyMenuOption {
     const DIFFICULTY_STEP: f64 = 0.1;
 
-    /// Creates and returns a new PlayerTypeMenuOption for the specified player
+    /// Creates and returns a new DifficultyMenuOption for the specified player
     pub fn new(player: ActivePlayer) -> Self
     {
         Self{player, selected_difficulty: 0.5}
